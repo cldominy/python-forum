81485e0b6c442ea22bf5ae86bce4f975ad9f32c8
Chris Dominy <cdominy@utexas.edu>
2021-06-07 11:07:26 -0500

    Updated login and logout API routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, jsonify
+import sys
+from flask import Blueprint, request, jsonify, session
+import sqlalchemy
 from app.models import User
 from app.db import get_db
 
@@ -8,14 +10,56 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 def signup():
   data = request.get_json()
   db = get_db()
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+  try:
+    # attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  #save in database 
-  db.add(newUser)
-  db.commit()
+    #save in database 
+    db.add(newUser)
+    db.commit()
+    print("Sucess!")
+    # insert failed, so rollback send error to front end 
+  except AssertionError:
+    print('Validation Error')
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
+  except sqlalchemy.exc.IntegrityError:
+    print ('mysql error')
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
 
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
   return jsonify(id = newUser.id)
+
+@bp.route('/users/logout', methods=[ 'POST' ])
+def logout():
+    # remove session variables 
+    session.clear()
+    return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
+
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  if user.verify_password(data['password']) == False:
+    return jsonify(message = 'Incorrect credentials'), 400
+  
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
+
+  return jsonify(id = user.id)
